ca7c451c3432a4dfc5dd150e96774ed731e8151a
Kateryna Ostrynska <kateriinag@gmail.com>
2022-10-08 21:22:42 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+let timerId = null;
+
+function onStartClick() {
+  refs.start.setAttribute('disabled', false);
+  refs.stop.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  refs.stop.setAttribute('disabled', false);
+  refs.start.removeAttribute('disabled');
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
