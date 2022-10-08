2328320d293df7f171da51661d11070ec0d52962
Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2022-10-08 18:23:03 +0000

    Deploying to gh-pages from @ Ostrynska/goit-js-hw-09@ca7c451c3432a4dfc5dd150e96774ed731e8151a 🚀

---
+++ 01-color-switcher.693b02b2.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.start.addEventListener("click",(function(){t.start.setAttribute("disabled",!1),t.stop.removeAttribute("disabled"),e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){t.stop.setAttribute("disabled",!1),t.start.removeAttribute("disabled"),clearInterval(e)}));let e=null;
+//# sourceMappingURL=01-color-switcher.693b02b2.js.map
